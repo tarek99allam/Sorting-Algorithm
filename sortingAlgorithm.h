c10e955f54ebc1b711bb5e13fa5ebafebbdcf5b3
Tarek Allam <tarek1999ahmed@gmail.com>
2022-11-17 01:24:45 +0200

    adding sorting progams

---
+++ sortingAlgorithm.h
@@ -0,0 +1,27 @@
+/* ********************************************************************************************
+/* @name     : SORTINGALGORITHM.C
+/* @brief    : WIRTE PROGRAMS OF SORTING ALFORITHM
+/* @author   : TAREK ALLAM
+/***********************************************************************************************/
+
+/*************************************** START INCLUDES ****************************************/
+#include "errorState.h"
+#include "stdTypes.h"
+
+/*************************************** END INCLUDES ******************************************/
+/*************************************** START DECLERATIONS ********************************************/
+ES_t showArray(uint32 arr[], uint32 size);
+
+ES_t sorting_By_Bubble(int arr[], int size);
+ES_t sorting_By_Selection(int arr[], int size);
+ES_t sorting_By_Bubble_secursive(int arr[], int size);
+ES_t sorting_By_Insertion(int arr[], int size);
+
+/*************************************** END DECLERATIONS **********************************************/
+
+/************************************************************************************************
+ * AUTHOR                 DATE                    BRIEF
+ ************************************************************************************************
+ * TAREK ALLAM         16 NOV 2022             write programs
+ *
+ */
